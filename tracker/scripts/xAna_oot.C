d5d11aeea5b361afef338080fb664043f778e6e6
Shin-Shan Yu <syu@ncu200.cern.ch>
2014-08-28 11:02:37 +0200

    fix the silly bug when determining if the hits are digitized

--- tracker/scripts/xAna_oot.C
+++ tracker/scripts/xAna_oot.C
@@ -158,7 +158,7 @@ void xAna_oot(std::string fin, float readoutWindow=3){ // readoutWindow default 
 
       for(int k=0; k < nBunches; k++)
 	{
-	  if(tdiff< (Float_t)(25*k+ readoutWindow) && tdiff  >= (Float_t)(25*k)){
+	  if(fabs(tdiff-25*k)< readoutWindow){
 	    if(decIndex==0 && subLayerIndex==0)nCount++;
 	    hr[decIndex][subLayerIndex]->Fill(k);
 	    break;
