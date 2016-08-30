769aed3d544aa24c1cefcce4dab686e978d6c4f0
syuvivida <syu@phy.ncu.edu.tw>
2016-08-30 19:17:58 +0200

    run multiple jobs at the same time

--- hgc/macros/runHistosAll.C
+++ hgc/macros/runHistosAll.C
@@ -5,14 +5,26 @@
 
 using namespace std;
 
-void runHistosAll(string particle,string energy, bool profile=false)
+void runHistosAll(string particle,string energy)
 {
-  string textFileName = profile? "profile_"+particle+energy+"_HighGain_LowGain_2D_type.dat":
-    particle+energy+"_HighGain_LowGain_2D_type.dat";
+  string textFileName = particle+energy+"_HighGain_LowGain_2D_type.dat";
   
   gSystem->Exec(Form("rm -rf %s",textFileName.data()));
   gSystem->Exec(Form("rm -rf slope_%s",textFileName.data()));
+
+  gSystem->Exec(Form("rm -rf profile_%s",textFileName.data()));
+  gSystem->Exec(Form("rm -rf slope_profile_%s",textFileName.data()));
   
+
+  textFileName = particle+energy+"_HighGain_LowGain_2D_commonmode_subtracted_type.dat";
+
+  gSystem->Exec(Form("rm -rf %s",textFileName.data()));
+  gSystem->Exec(Form("rm -rf slope_%s",textFileName.data()));
+
+  gSystem->Exec(Form("rm -rf profile_%s",textFileName.data()));
+  gSystem->Exec(Form("rm -rf slope_profile_%s",textFileName.data()));
+
+
   string inputDir="rootfiles/"+particle+"/"+energy;
   gSystem->Exec("rm -rf inputrootfiles.dat");
   string name = "ls -1 "+inputDir+"/*root > inputrootfiles.dat";
@@ -26,7 +38,15 @@ void runHistosAll(string particle,string energy, bool profile=false)
       string fileName;
       fin >> fileName;
       if(!fin.eof())
-	computeSaturation(fileName,profile);
+	{
+	  // default histograms with only pedestal subtraction
+	  computeSaturation(fileName,false);
+	  computeSaturation(fileName,true);
+	  // add histograms that subtract common mode noise
+	  computeSaturation(fileName,false,"HighGain_LowGain_2D_commonmode_subtracted_type");
+	  computeSaturation(fileName,true,"HighGain_LowGain_2D_commonmode_subtracted_type");
+
+	}
     }
 
 
