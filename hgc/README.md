292b4e0606b5a231504c5063a575f605b403c595
syuvivida <syu@phy.ncu.edu.tw>
2016-09-17 17:09:08 +0200

    codes for 8-layer HGC

--- hgc/README.md
+++ hgc/README.md
@@ -4,32 +4,20 @@ Go to a cms evironment enabled area(lxplus)
 
 ```
 cmsrel CMSSW_8_0_1
-cd CMSSW_8_0_1/src
-git cms-init
-git clone https://github.com/CMS-HGCAL/TestBeam HGCal/
-cd HGCal
-git checkout FNAL_TB_16Layers
-git pull
-rm -f RawToDigi/plugins/DigiPlotter_New.cc
-cp ~rchatter/public/Ped_*Gain_L*.txt CondObjects/data
-cp ~rchatter/public/Layer_Sum_Analyzer.cc Reco/plugins
-cp ~rchatter/public/test_cfg.py .
+cd CMSSW_8_0_1/src/
+git clone git@github.com:CMS-HGCAL/TestBeam.git HGCal
 ```
 
-### Change default parameters
-
-1) Open Reco/python/hgcaltbrechitproducer_cfi.py
-Change the two pedestal files to Ped_HighGain_L1.txt and Ped_LowGain_L1.txt
-
-2) Open Geometry/interface/HGCalTBGeometryParameters.h
----> Change MAXLAYERs to 1 and MAXSKIROCs to 2
-
-3) Open Geometry/interface/HGCalTBSpillParameters.h
----> Change Spill to as many you wish to analyze(Number of events in the file should be < Events_Per_Spills*Number_Of_Spills)
 
 ### Compile 
 
-scram b -j32
+scramv1 b -j 8
+
+### To run the code
+
+cd HGCalc
+
+msRun test_cfg.py nSpills=15 chainSequence=4 pedestalsHighGain=CondObjects/data/Ped_HighGain_L8.txt pedestalsLowGain=CondObjects/data/Ped_LowGain_L8.txt runNumber=1058 runType=HGCRun particle=electron energy=250GeV
 
 ### Meaning of each analyzer
 
@@ -43,4 +31,6 @@ scram b -j32
 
 Can activate all 3 at the same time.
 
-The output root file produced is HGC_Output_Reco.root
+For now, we only activate 3) to reduce the running time
+
+
--- hgc/python/test_cfg.py
+++ hgc/python/test_cfg.py
@@ -1,89 +1,148 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+import os,sys
+
+options = VarParsing.VarParsing('standard') # avoid the options: maxEvents, files, secondaryFiles, output, secondaryOutput because they are already defined in 'standard'
+
+options.register('dataFolder',
+                 '/afs/cern.ch/work/r/rslu/public/HGC_TB_data_Sep2016/',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 'folder containing raw text input')
+
+options.register('runNumber',
+                 850,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 'Input file to process')
+
+options.register('runType',
+                 'Unknown',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 'Type of run: HGCRun for run with beam on, PED for pedestal run, Unknown otherwise')
 
 
-from FWCore.ParameterSet.VarParsing import VarParsing
-options = VarParsing ('analysis')
-options.register ('RUNNUMBER',
-                  '395',
-                  VarParsing.multiplicity.singleton,
-                  VarParsing.varType.string,
-                  "RUNNUMBER")
+options.register('chainSequence',
+                 0,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 '0: if runType is PED then do Digi, if runType is HGC_Run then do Digi and Reco (not implemented yet); 1: do Digi; 2: only Reco (not implemented yet); 3: Digi + highgain_correlation_cm; 4: event display sequence; 5: highgain_correlation_cm + event display sequence')
+
+options.register('nSpills',
+                 15,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 'Number of spills in run')
+
+options.register('pedestalsHighGain',
+                 'CondObjects/data/Ped_HighGain_L8.txt',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 'Path to high gain pedestals file')
+
+options.register('pedestalsLowGain',
+                 'CondObjects/data/Ped_LowGain_L8.txt',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 'Path to low gain pedestals file')
 
 
-options.register ('PARTICLE',
+options.register ('particle',
                   'electron',
-                  VarParsing.multiplicity.singleton,
-                  VarParsing.varType.string,
-                  "PARTICLE")
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "particle type")
 
 
-options.register ('ENERGY',
+options.register ('energy',
                   '20GeV',
-                  VarParsing.multiplicity.singleton,
-                  VarParsing.varType.string,
-                  "ENERGY")
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "energy of beam")
+
+
+## This output is disabled since we do not need edm root files (Eiko)
+options.output = "test_output.root"
 
 options.parseArguments()
 
+# print options # <--- A better option is to call test_cfg is called with "print" argument e.g. cmsRun test_cfg.py print dataFolder=example1 outputFolder=example2 ...
 
+if not os.path.isdir(options.dataFolder):
+    sys.exit("Error: Data folder not found or inaccessible!")
+
+directory=options.particle+"/"+options.energy
+if not os.path.isdir(directory):
+    os.system("mkdir -p " + directory)
+
+if (options.runType != "PED" and options.runType != "HGCRun"):
+    sys.exit("Error: only runtypes PED and HGCRun supported for now; given runType was %s"%(options.runType))
+
+if (options.runType == "PED"):
+    if (os.path.isfile(options.pedestalsHighGain) or os.path.isfile(options.pedestalsLowGain)):
+        sys.exit("Error: Run %d is a pedestals run. The arguments pedestalsHighGain = %s and pedestalsLowGain = %s should be paths that do not lead to an existing file."%(options.runNumber, options.pedestalsHighGain, options.pedestalsLowGain))
+            
+
+
+################################
 process = cms.Process("unpack")
-process.load('HGCal.RawToDigi.hgcaltbdigis_cfi')
-process.load('HGCal.RawToDigi.hgcaltbdigisplotter_cfi')
-process.load('HGCal.Reco.hgcaltbrechitproducer_cfi')
-process.load('HGCal.Reco.hgcaltbrechitplotter_cfi')
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
 
-
-
-import os
-   
-directory=options.PARTICLE
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-directory=options.PARTICLE+"/"+options.ENERGY
-if not os.path.exists(directory):
-    os.makedirs(directory)
+####################################
+process.load('HGCal.StandardSequences.RawToDigi_cff')
+process.load('HGCal.StandardSequences.LocalReco_cff')
+process.load('HGCal.StandardSequences.dqm_cff')
 
 
 process.source = cms.Source("HGCalTBTextSource",
-                            run=cms.untracked.int32(1),####provide file name below
-                            fileNames=cms.untracked.vstring(
-        "file:/afs/cern.ch/work/s/syu/hgcal/data/"+options.PARTICLE+"/"+options.ENERGY+"/HGCRun_Output_000"+options.RUNNUMBER+".txt") 
-
-
-
-##
-# here a vector is provided, but in the .cc only the first one is used TO BE FIXED
-#                            fileNames=cms.untracked.vstring("file:/afs/cern.ch/work/r/rslu/public/HGC_TB_data_Aug2016/PED_Output_000111.txt")
+                            run=cms.untracked.int32(options.runNumber), ### maybe this should be read from the file
+                            #fileNames=cms.untracked.vstring("file:Raw_data_New.txt") ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
+                            fileNames=cms.untracked.vstring("file:%s/%s_Output_%06d.txt"%(options.dataFolder,options.runType,options.runNumber)), ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
+                            nSpills=cms.untracked.uint32(options.nSpills),
 )
+
+
+
+######
+process.hgcaltbdigisplotter.pedestalsHighGain = cms.untracked.string(options.pedestalsHighGain)
+process.hgcaltbdigisplotter.pedestalsLowGain  = cms.untracked.string(options.pedestalsLowGain)
+
+process.hgcaltbrechits.pedestalLow = cms.string(options.pedestalsLowGain)
+process.hgcaltbrechits.pedestalHigh = cms.string(options.pedestalsHighGain)
+process.hgcaltbrechits.gainLow = cms.string('')
+process.hgcaltbrechits.gainHigh = cms.string('')
 
 process.dumpRaw = cms.EDAnalyzer("DumpFEDRawDataProduct",
                               dumpPayload=cms.untracked.bool(True))
 
 process.dumpDigi = cms.EDAnalyzer("HGCalDigiDump")
 
+process.LayerSumAnalyzer.particleType = cms.string(options.particle)
+
 
 process.output = cms.OutputModule("PoolOutputModule",
-			fileName = cms.untracked.string("output_"+options.RUNNUMBER+".root")
-                                  )
+			fileName = cms.untracked.string(options.output)
+                                 )
 
-#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_Cluster_"+RUNNUMBER+".root")) ### Analyzed output file with histograms
-#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_Reco_352.root")) ### Analyzed output file with histograms
-process.TFileService = cms.Service("TFileService", fileName = cms.string(directory+"/"+options.PARTICLE+options.ENERGY+"_HGC_Output_EventDisplay_"+options.RUNNUMBER+".root")) ### Analyzed output file with histograms
-
-process.LayerSumAnalyzer.particleType = cms.string(options.PARTICLE)
-
-########Activate this to produce event displays#########################################
-#process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.hgcaltbrechitsplotter_highgain_new)
-
-################Not needed for DQM purposes, produces digi histograms for each channel, and the pedestal txt file needed for Digi->Reco
-#process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbdigisplotter)
-
-################Produces Reco histograms for each channel as well as a scatter plot of the Reco per channel#############
-#process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.hgcaltbrechitsplotter_highgain_correlation_cm)
-
-#################Produces Clusters of Recos(7cells, 19cells and all cells(full hexagons only))################
-process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.LayerSumAnalyzer)
+if (options.chainSequence == 1):
+    process.TFileService = cms.Service("TFileService", fileName = cms.string("%s/%s_Output_%06d_Digi.root"%(options.outputFolder,options.runType,options.runNumber)))
+elif (options.chainSequence == 3 or options.chainSequence == 4 or options.chainSequence == 5):
+    process.TFileService = cms.Service("TFileService", fileName = cms.string("%s/%s_%s_Output_%06d_Reco.root"%(directory,options.particle+options.energy,options.runType,options.runNumber)))
 
 
+if (options.chainSequence == 1):
+    process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbdigisplotter)
+elif (options.chainSequence == 3):
+    process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.hgcaltbrechitsplotter_highgain_correlation_cm)
+elif (options.chainSequence == 4): 
+    process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.LayerSumAnalyzer)
+elif (options.chainSequence == 5):
+    process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.hgcaltbrechitsplotter_highgain_correlation_cm*process.hgcaltbrechitsplotter_highgain_new)
+
+
+## Disabled since we do not need EDM root files (Eiko)
 #process.end = cms.EndPath(process.output)
